b144737801a7dc4ed01e805ed7ccaa9bcf884182
RobinDeClerck <57659852+RobinDeClerck1@users.noreply.github.com>
2020-12-29 01:19:02 +0100

    Added db connection

--- application.py
+++ application.py
@@ -1,12 +1,42 @@
 import web
 
+DATABASE_HOST="database"
+DATABASE_DB="shippingChallenge"
+DATABASE_PORT="3306"
+DATABASE_USER="root"
+DATABASE_PASSWORD="root"
+
+def setup_database_connection():
+    # CONNECTION TO SQL SERVER #
+    import mysql.connector
+
+    mydb = mysql.connector.connect(
+        user=DATABASE_USER,
+        passwd=DATABASE_PASSWORD,
+
+        host=DATABASE_HOST,
+        port=DATABASE_PORT,
+
+        database=DATABASE_DB,
+    )
+    return mydb
+
 urls = (
     '/', 'index'
 )
 
 class index:
     def GET(self):
-        return "Hello, world!"
+        # DB Connection
+        database = setup_database_connection()
+        cursor = database.cursor()
+
+        db_query = 'SELECT surname FROM User WHERE userId = 1'
+        cursor.execute(db_query)
+
+        surname = cursor.fetchone()[0]
+
+        return 'Hi ' + str(surname)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
